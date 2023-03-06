1f49ab93cd5520fe76a7d92583763fc2ab5bbd55
Hamilton <hamilton.gomes8@hotmail.com>
2023-03-06 20:45:02 -0300

    dividindo em funções

--- js/form.js
+++ js/form.js
@@ -4,11 +4,28 @@ botaoAdicionar.addEventListener('click', function (event) {
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    var pacienteNovo = criandoPacientes(form);
 
+    var pacienteTr = adicionandoTr(pacienteNovo)
+
+    var tabelaPacientes = document.querySelector("#tabela-pacientes")
+
+    tabelaPacientes.appendChild(pacienteTr)
+})
+
+function criandoPacientes(form) {
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaIMC(form.peso.value, form.altura.value)
+    }
+
+    return paciente
+}
+
+function adicionandoTr(paciente) {
     var pacienteTr = document.createElement('tr')
 
     var tdNome = document.createElement('td')
@@ -16,12 +33,12 @@ botaoAdicionar.addEventListener('click', function (event) {
     var tdAltura = document.createElement('td')
     var tdGordura = document.createElement('td')
     var tdIMC = document.createElement('td')
-    
-    tdNome.textContent = nome
-    tdPeso.textContent = peso
-    tdAltura.textContent = altura
-    tdGordura.textContent = gordura
-    tdIMC.textContent = calculaIMC(peso, altura)
+
+    tdNome.textContent = paciente.nome;
+    tdPeso.textContent = paciente.peso;
+    tdAltura.textContent = paciente.altura;
+    tdGordura.textContent = paciente.gordura;
+    tdIMC.textContent = paciente.imc;
 
     pacienteTr.appendChild(tdNome)
     pacienteTr.appendChild(tdPeso)
@@ -29,8 +46,5 @@ botaoAdicionar.addEventListener('click', function (event) {
     pacienteTr.appendChild(tdGordura)
     pacienteTr.appendChild(tdIMC)
 
-    var tabelaPacientes = document.querySelector("#tabela-pacientes")
-
-    tabelaPacientes.appendChild(pacienteTr)
-
-})
+    return pacienteTr;
+}
